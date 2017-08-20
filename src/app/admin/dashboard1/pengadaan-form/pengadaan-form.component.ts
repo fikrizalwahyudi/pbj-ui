d008a1ae352f7d86b14fb819fdd1da966a89d949
iroy <indrataroy@gmail.com>
2017-08-20 19:04:56 +0700

    penambahan dashboard dan form pengadaan

---
+++ src/app/admin/dashboard1/pengadaan-form/pengadaan-form.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-pengadaan-form',
+  templateUrl: './pengadaan-form.component.html',
+  styleUrls: ['./pengadaan-form.component.css']
+})
+export class PengadaanFormComponent implements OnInit {
+  data={};
+  constructor(private router:Router) { }
+
+  ngOnInit() {
+  }
+
+  keList(){
+    this.router.navigateByUrl('admin/dashboard')
+  }
+
+}
