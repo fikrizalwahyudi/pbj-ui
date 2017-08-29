c253733806e7add4222079ef6b93d563cde629bf
Rana Lubis <rana.lubis@gmail.com>
2017-08-29 16:49:14 +0700

    trying test script

--- src/app/admin/master/penyedia/penyedia-form/penyedia-form.component.spec.ts
+++ src/app/admin/master/penyedia/penyedia-form/penyedia-form.component.spec.ts
@@ -1,6 +1,12 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
+import { MockBackend, MockConnection } from '@angular/http/testing';
 
 import { PenyediaFormComponent } from './penyedia-form.component';
+import { CommonModule } from '@angular/common';
+import { ShareModule } from '../../../../share/share.module';
+import { FormsModule } from '@angular/forms';
+import { Http, BaseRequestOptions } from '@angular/http';
 
 describe('PenyediaFormComponent', () => {
   let component: PenyediaFormComponent;
@@ -8,7 +14,22 @@ describe('PenyediaFormComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ PenyediaFormComponent ]
+      declarations: [ PenyediaFormComponent ],
+      imports: [
+        ShareModule,
+        FormsModule,
+        RouterTestingModule
+      ],
+      providers: [
+        {
+          provide: Http, useFactory:(backend, options) => {
+            return new Http(backend, options);
+          },
+          deps: [MockBackend, BaseRequestOptions]
+        },
+        MockBackend,
+        BaseRequestOptions
+      ]
     })
     .compileComponents();
   }));
--- src/app/app.component.spec.ts
+++
@@ -1,9 +0,0 @@
-import { TestBed } from '@angular/core/testing';
-import { AppModule } from './app.module';
-
-describe("Application Test", () => {
-	// async beforeEach
-	beforeEach(() => {
-		
-	});	
-});
